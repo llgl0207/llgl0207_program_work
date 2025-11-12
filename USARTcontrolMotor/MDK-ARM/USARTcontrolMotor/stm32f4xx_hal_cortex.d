917208758d0d35cc964cf937fe0328331a01b0fe
浪里个浪 <120241584+llgl0207@users.noreply.github.com>
2025-11-12 21:37:49 +0800

    update

---
+++ USARTcontrolMotor/MDK-ARM/USARTcontrolMotor/stm32f4xx_hal_cortex.d
@@ -0,0 +1,30 @@
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Core/Inc/stm32f4xx_hal_conf.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Include/core_cm4.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Include/cmsis_version.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+usartcontrolmotor\stm32f4xx_hal_cortex.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
